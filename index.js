5f2a5b7165462987fbc61033754c29e9dc06031c
Almaz <Almazshaehov777@gmail.com>
2026-04-04 18:41:02 +0300

    исправлены главная страница, "Обо мне", "Портфолио" на всех устройствах

--- index.js
+++ index.js
@@ -13,20 +13,3 @@ function cardClick(index){
 for(let i = 0; i<cards.length; ++i){
     cards[i].onclick = (event) => cardClick(i, event);
 }
-
-
-const cards2 = document.querySelectorAll('.cardD');
-
-const link = [
-    'https://github.com/almazshaehov777-hub/Audio-Player',
-  'https://github.com/almazshaehov777-hub/Registrate-Window', 
-  'https://github.com/almazshaehov777-hub/Memorizer'
-];
-
-function cardsClick(index){
-    window.open(link[index], '_blank');
-}
-
-for(let i = 0; i<cards2.length; ++i){
-    cards2[i].onclick = (event) => cardsClick(i, event);
-}
